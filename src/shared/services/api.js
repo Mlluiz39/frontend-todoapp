c7ba57f87f17f79ec10594d3098fd64aaa3fea9b
Marcelo Luiz <mlluizpereira39@gmail.com>
2025-01-06 09:52:38 -0300

    update API base URL to include '/api' path

--- src/shared/services/api.js
+++ src/shared/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://backendtodoapp-3.onrender.com/'
+  baseURL: 'https://backendtodoapp-3.onrender.com/api',
 })
 
 api.interceptors.request.use(async config => {
